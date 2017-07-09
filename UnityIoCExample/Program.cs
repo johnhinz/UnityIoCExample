8ec2ef8900325cbc4a5871c2c4f54d02d28d6841
John Hinz <jhinz402@hotmail.com>
2017-07-09 15:56:42 -0400

    Initial commit of Unity IoC Example

--- UnityIoCExample/Program.cs
+++ UnityIoCExample/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,50 @@ namespace UnityIoCExample
     {
         static void Main(string[] args)
         {
+            // initilize the Unity IoC container
+            UnityContainer container = new UnityContainer();
+            // load IoC configuration from App.Config
+            container.LoadConfiguration();
+            // resolve a dependent class
+            Dependent dependency = container.Resolve<Dependent>();
+
+            dependency.DoSomething();
+            Console.ReadLine();
+        }
+    }
+
+    public class Dependent
+    {
+        private IDependency _dependency;
+
+        public Dependent(IDependency dependency)
+        {
+            _dependency = dependency;
+        }
+        public void DoSomething()
+        {
+            _dependency.DoSomeWork();
+        }
+    }
+
+    public interface IDependency
+    {
+        void DoSomeWork();
+    }
+
+    public class DependencyOne : IDependency
+    {
+        public void DoSomeWork()
+        {
+            Console.WriteLine("DependencyOne doing work");
+        }
+    }
+
+    public class DependencyTwo : IDependency
+    {
+        public void DoSomeWork()
+        {
+            Console.WriteLine("DependencyTwo doing some work");
         }
     }
 }
